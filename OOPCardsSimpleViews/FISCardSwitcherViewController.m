d203b756c2f8081c34b66c53ae5141338ff06f67
imryan <notryancohen@gmail.com>
2016-06-16 13:33:57 -0400

    finished

--- OOPCardsSimpleViews/FISCardSwitcherViewController.m
+++ OOPCardsSimpleViews/FISCardSwitcherViewController.m
@@ -4,22 +4,29 @@
 
 @interface FISCardSwitcherViewController ()
 
+@property (weak, nonatomic) IBOutlet UILabel *topLabel;
+@property (weak, nonatomic) IBOutlet UILabel *middleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *bottomLabel;
+- (IBAction)cardSelected:(id)sender;
+
 @end
 
 @implementation FISCardSwitcherViewController
 
-- (void)viewDidLoad
-{
+- (IBAction)cardSelected:(id)sender {
+    UIButton *cardButton = (UIButton *)sender;
+    self.topLabel.text = cardButton.titleLabel.text;
+    self.middleLabel.text = cardButton.titleLabel.text;
+    self.bottomLabel.text = cardButton.titleLabel.text;
+}
+
+- (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
-    self.threeOfSpades = [[FISCard alloc] initWithSuit:@"♠️"
-                                                  rank:@"3"];
-    self.fourOfClubs = [[FISCard alloc] initWithSuit:@"♣️"
-                                                rank:@"4"];
-    self.eightOfDiamonds = [[FISCard alloc] initWithSuit:@"♦️"
-                                                    rank:@"8"];
-    self.tenOfHearts = [[FISCard alloc] initWithSuit:@"♥️"
-                                                rank:@"10"];
+    
+    self.threeOfSpades = [[FISCard alloc] initWithSuit:@"♠️" rank:@"3"];
+    self.fourOfClubs = [[FISCard alloc] initWithSuit:@"♣️" rank:@"4"];
+    self.eightOfDiamonds = [[FISCard alloc] initWithSuit:@"♦️" rank:@"8"];
+    self.tenOfHearts = [[FISCard alloc] initWithSuit:@"♥️" rank:@"10"];
 }
 
 @end
